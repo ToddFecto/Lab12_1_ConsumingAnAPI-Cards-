c8b3cb6ea3d1f542eef1d11dbb60758536294c29
Todd Fecto <toddfecto@quickenloan.com>
2021-09-28 08:19:29 -0400

    Needs a lot of work to build API

---
+++ Lab12_1_ConsumingAnAPI/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using Lab12_1_ConsumingAnAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Lab12_1_ConsumingAnAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> shufflethecards(string key)
+        {
+            string domain = "https://deckofcardsapi.com";
+            string path = $"/api/deck/{key}.json";
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(domain);
+            var connection = await client.GetAsync(path); // Async means our function will pause and wait until GetAsync finishes. Then come back and pick up where we left off.
+            CardDeck result = await connection.Content.ReadAsAsync<CardDeck>();
+
+            return View(result);
+        }
+
+        public async Task<IActionResult> drawacard(string key)
+        {
+            string domain = "https://deckofcardsapi.com";
+            string path = $"/api/deck/{key}.json";
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(domain);
+            var connection = await client.GetAsync(path); // Async means our function will pause and wait until GetAsync finishes. Then come back and pick up where we left off.
+            CardDeck result = await connection.Content.ReadAsAsync<CardDeck>();
+
+            return View(result);
+        }
+
+        public async Task<IActionResult> reshufflethecards(string key)
+        {
+            string domain = "https://deckofcardsapi.com";
+            string path = $"/api/deck/{key}.json";
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(domain);
+            var connection = await client.GetAsync(path); // Async means our function will pause and wait until GetAsync finishes. Then come back and pick up where we left off.
+            CardDeck result = await connection.Content.ReadAsAsync<CardDeck>();
+
+            return View(result);
+        }
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lab12_1_ConsumingAnAPI/Models/CardDeck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab12_1_ConsumingAnAPI.Models
+{
+    public class CardDeck
+    {
+        public bool success { get; set; }
+        public string deck_id { get; set; }
+        public int remaining { get; set; }
+        public bool shuffled { get; set; }
+        public int deck_count { get; set; }
+    }
+}
